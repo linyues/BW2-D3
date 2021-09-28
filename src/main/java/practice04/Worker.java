5a5d97870b2e5a275f3a8d11f50d499a69d63f75
linyues <2694166721@qq.com>
2021-09-28 14:52:35 +0800

    P4 finished

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,27 @@
 package practice04;
 
-public class Worker {
+import practice03.Person;
+
+public class Worker extends Person {
+    private String name;
+    private int age;
+
+    public Worker(String name,int age){
+        super("Tom",21);
+        this.name = name ;
+        this.age = age;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+    public String introduce(){
+        return super.introduce()+ " I am a Worker. I have a job.";
+    }
 }
