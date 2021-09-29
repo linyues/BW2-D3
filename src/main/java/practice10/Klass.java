70a5efcdf295e24b0d95a2176dfdd7ae6a03cf95
linyues <2694166721@qq.com>
2021-09-29 17:45:13 +0800

    P10 finished

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,47 @@
 package practice10;
 
 public class Klass {
+    private int number;
+    private Student leader;
+    private String displayName;
+
+    public Klass(int number) {
+        this.number = number;
+        this.displayName = "Class " + this.number;
+    }
+
+    public void assignLeader(Student student) {
+        if(student.getKlass().number != this.number){
+            System.out.print("It is not one of us.\n");
+        }else {
+            this.leader = student;
+        }
+    }
+
+    public void appendMember(Student student) {
+        student.getKlass().setNumber(this.number);
+    }
+
+    public boolean isIn(Student student) {
+        if(this.number == student.getKlass().getNumber()){
+            return true;
+        }
+        return false;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
